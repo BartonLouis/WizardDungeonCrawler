be5e6c2e8f41c77f7718c0c88733bb390ece35fe
LouisTSAS <louis@tandemsetandscenery.com>
2024-12-04 16:32:47 +0000

    Added Room Colliders

--- Assets/Scripts/MapGeneration/DungeonGenerator.cs
+++ Assets/Scripts/MapGeneration/DungeonGenerator.cs
@@ -1,9 +1,18 @@
+using Louis.Patterns.ServiceLocator;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace DungeonGeneration {
+    public interface IDungeonGeneratorService : IService {
+        public Transform Transform { get; }
+        public void Generate(int seed);
+        public void Generate();
+    }
+
     [RequireComponent(typeof(TilemapVisualiser))]
-    public class DungeonGenerator : MonoBehaviour {
+    public class DungeonGenerator : MonoBehaviour, IDungeonGeneratorService {
+
+        public Transform Transform => transform;
         [Header("Settings")]
         [SerializeField] int _seed;
 
@@ -12,12 +21,32 @@ namespace DungeonGeneration {
 
         TilemapVisualiser _visualiser;
 
+        private void OnEnable() {
+            ServiceLocator.Register<IDungeonGeneratorService>(this);
+        }
+
+        private void OnDisable() {
+            ServiceLocator.Deregister<IDungeonGeneratorService>(this);
+        }
+
         public void Generate(int seed) {
             _seed = seed;
             Generate();
         }
 
         public void Generate() {
+            ServiceLocator.Register<IDungeonGeneratorService>(this);
+            if (Application.isPlaying) {
+                foreach (Transform child in transform) {
+                    Destroy(child.gameObject);
+                }
+            } else {
+                for (int i = transform.childCount; i > 0; --i) {
+                    DestroyImmediate(transform.GetChild(0).gameObject);
+                }
+            }
+
+
             DungeonInfo dungeon = new DungeonInfo(_seed, Vector2Int.zero);
             foreach (var step in _generationSteps) {
                 step.Generate(dungeon);
@@ -25,7 +54,7 @@ namespace DungeonGeneration {
 
             _visualiser = GetComponent<TilemapVisualiser>();
             _visualiser.Clear();
-            foreach(var tile in dungeon) {
+            foreach (var tile in dungeon) {
                 _visualiser.PaintSingleTile(new Vector2Int(tile.x, tile.y), tile.layer);
             }
         }
